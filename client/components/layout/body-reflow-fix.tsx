51baaad540cde5fd79b07b07a72c7a616200ca2f
Asabeam <sharper.edge.tech@gmail.com>
2025-11-02 18:27:54 +0900

    update

--- client/components/layout/body-reflow-fix.tsx
+++ client/components/layout/body-reflow-fix.tsx
@@ -7,6 +7,10 @@ export function BodyReflowFix() {
     document.body.style.display = 'none';
     document.body.offsetHeight; // Force reflow
     document.body.style.display = '';
+
+    if (/Safari/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent)) {
+      window.dispatchEvent(new Event("resize"));
+    }
   }, []);
 
   return null;
